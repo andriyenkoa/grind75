59342ed58b2627ba7a14eaa5f70bf42a351fb78e
Andrey Andriyenko <amandriyenko@gmail.com>
2024-07-25 11:34:41 +0500

    Contains Duplicate

---
+++ Week 2/24. Contains Duplicate/contains_duplicate.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        checked = set()
+
+        for num in nums:
+            if num in checked:
+                return True
+            checked.add(num)
+
+        return False
