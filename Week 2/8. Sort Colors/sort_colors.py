e7e50128b4dcb02e16049d00f005f481462b123f
Andrey Andriyenko <amandriyenko@gmail.com>
2024-08-13 13:38:11 +0500

    Sort Colors

---
+++ Week 2/8. Sort Colors/sort_colors.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, mid, right = 0, 0, len(nums) - 1
+        while mid <= right:
+            if nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+            elif nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
